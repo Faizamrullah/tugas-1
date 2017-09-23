2e17bdeddd4ee133e971befdbdcfebd4c616ee5a
Faizamrullah <31976838+Faizamrullah@users.noreply.github.com>
2017-09-23 00:30:52 -0700

    Add files via upload
    data diri

---
+++ tugas1-04217052.py
@@ -0,0 +1,9 @@
+nama = "moh faaiz amrullah"
+nim = "04217052"
+umur = 18
+print ("namaku:" + nama)
+print ("nimku:" + nim)
+print ( "umur" )
+print (umur)
+print ("umur 5 tahun kedepan")
+print (umur +5)
